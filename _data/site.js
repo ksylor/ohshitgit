890c44c4abc984158bb6e94220cc61c206a98253
Katie Sylor-Miller <ksylormiller@etsy.com>
2020-02-27 13:02:55 -0500

    alphabetical order and updated copyright

--- _data/site.js
+++ _data/site.js
@@ -2,27 +2,22 @@ module.exports = function() {
     return {
         "url_swears": "https://ohshitgit.com",
         "url_noswears": "https://dangitgit.com",
-        "copyright": "&copy; 2016-2019 Katie Sylor-Miller",
+        "copyright": "&copy; 2016-2020 Katie Sylor-Miller",
         "languages": [
-            {
-                "label": "english",
-                "code": "en",
-                "arialabel": "View in other languages"
-            },
             {
                 "label": "deutsch",
                 "code": "de",
                 "arialabel": "In anderen Sprachen anzeigen"
             },
             {
-                "label": "русский",
-                "code": "ru",
-                "arialabel": "Посмотреть на других языках"
+                "label": "english",
+                "code": "en",
+                "arialabel": "View in other languages"
             },
             {
-                "label": "polski",
-                "code": "pl",
-                "arialabel": "Zobacz w innych językach"
+                "label": "français",
+                "code": "fr",
+                "arialabel": "Choisir une autre langue"
             },
             {
                 "label": "italiano",
@@ -30,9 +25,14 @@ module.exports = function() {
                 "arialabel": "Vedi in altre lingue"
             },
             {
-                "label": "français",
-                "code": "fr",
-                "arialabel": "Choisir une autre langue"
+                "label": "polski",
+                "code": "pl",
+                "arialabel": "Zobacz w innych językach"
+            },
+            {
+                "label": "русский",
+                "code": "ru",
+                "arialabel": "Посмотреть на других языках"
             }
         ],
         "twitter_ksylor": `<a href="https://twitter.com/ksylor">@ksylor</a>`,
