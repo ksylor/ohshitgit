bc7f92fd961838a3224d128deebb448135d74048
Katie Sylor-Miller <ksylor@post.harvard.edu>
2020-02-27 22:41:34 -0500

    Various cleanup things (#11)
    * alphabetical order and updated copyright
    
    * fallback to using sort filter
    
    * refactor structure of the footer and outro, add automatic output of list of collaborators
    
    * update french translations with new structure
    
    * update italian to use new structure
    
    * update italian and polish
    
    * update russian language
    
    * update german with new structure
    
    * update readme with new instructions
    
    * add codeowners
    
    * redirect any sub-request urls
    
    * more redirects

--- _data/site.js
+++ _data/site.js
@@ -2,27 +2,22 @@ module.exports = function() {
     return {
         "url_swears": "https://ohshitgit.com",
         "url_noswears": "https://dangitgit.com",
-        "copyright": "&copy; 2016-2019 Katie Sylor-Miller",
+        "copyright": "&copy; 2016-2020 Katie Sylor-Miller",
         "languages": [
-            {
-                "label": "english",
-                "code": "en",
-                "arialabel": "View in other languages"
-            },
             {
                 "label": "deutsch",
                 "code": "de",
                 "arialabel": "In anderen Sprachen anzeigen"
             },
             {
-                "label": "русский",
-                "code": "ru",
-                "arialabel": "Посмотреть на других языках"
+                "label": "english",
+                "code": "en",
+                "arialabel": "View in other languages"
             },
             {
-                "label": "polski",
-                "code": "pl",
-                "arialabel": "Zobacz w innych językach"
+                "label": "français",
+                "code": "fr",
+                "arialabel": "Choisir une autre langue"
             },
             {
                 "label": "italiano",
@@ -30,12 +25,42 @@ module.exports = function() {
                 "arialabel": "Vedi in altre lingue"
             },
             {
-                "label": "français",
-                "code": "fr",
-                "arialabel": "Choisir une autre langue"
+                "label": "polski",
+                "code": "pl",
+                "arialabel": "Zobacz w innych językach"
+            },
+            {
+                "label": "русский",
+                "code": "ru",
+                "arialabel": "Посмотреть на других языках"
             }
         ],
-        "twitter_ksylor": `<a href="https://twitter.com/ksylor">@ksylor</a>`,
-        "twitter_ohshitgit": `<a href="https://twitter.com/ohshitgit">@ohshitgit`
+        "collaborators": [
+            {
+                "name": "Moritz Stückler",
+                "link": "https://github.com/pReya",
+                "lang": "de"
+            },
+            {
+                "name": "Daniil Golubev",
+                "link": "https://github.com/dadyarri",
+                "lang": "ru"
+            },
+            {
+                "name": "Łukasz Wójcik",
+                "link": "https://github.com/lwojcik",
+                "lang": "pl"
+            },
+            {
+                "name": "fedemcmac",
+                "link": "https://github.com/fedemcmac",
+                "lang": "it"
+            },
+            {
+                "name": "Michel",
+                "link": "https://github.com/michelc",
+                "lang": "fr"
+            }
+        ]
     };
 };
