878111f74dfa316d5013abc1638e01137aae009f
Allie Jones <allie@alliejon.es>
2021-02-28 10:23:57 -0500

    Prettier setup (#91)
    * Install and configure prettier
    
    * Install pretty-quick and husky to format on commit
    
    * Run prettier on everything
    
    * re-run prettier after fixing merge conflicts
    
    Co-authored-by: Katie Sylor-Miller <ksylormiller@etsy.com>

--- _data/site.js
+++ _data/site.js
@@ -1,300 +1,300 @@
 module.exports = {
-    "url_swears": "https://ohshitgit.com",
-    "url_noswears": "https://dangitgit.com",
-    "copyright": "&copy; 2016-2021 Katie Sylor-Miller",
-    "languages": [
+    url_swears: "https://ohshitgit.com",
+    url_noswears: "https://dangitgit.com",
+    copyright: "&copy; 2016-2021 Katie Sylor-Miller",
+    languages: [
         {
-            "label": "afrikaans",
-            "code": "af",
-            "arialabel": "Sien die webblad in ander tale"
+            label: "afrikaans",
+            code: "af",
+            arialabel: "Sien die webblad in ander tale",
         },
         {
-            "label": "বাংলা",
-            "code": "bn",
-            "arialabel": "অন্যান্য ভাষায় দেখুন"
+            label: "বাংলা",
+            code: "bn",
+            arialabel: "অন্যান্য ভাষায় দেখুন",
         },
         {
-            "label": "česky",
-            "code": "cs",
-            "arialabel": "Ostatní jazyky"
+            label: "česky",
+            code: "cs",
+            arialabel: "Ostatní jazyky",
         },
         {
-            "label": "deutsch",
-            "code": "de",
-            "arialabel": "In anderen Sprachen anzeigen"
+            label: "deutsch",
+            code: "de",
+            arialabel: "In anderen Sprachen anzeigen",
         },
         {
-            "label": "english",
-            "code": "en",
-            "arialabel": "View in other languages"
+            label: "english",
+            code: "en",
+            arialabel: "View in other languages",
         },
         {
-            "label": "español",
-            "code": "es",
-            "arialabel": "Mira en otro idioma"
+            label: "español",
+            code: "es",
+            arialabel: "Mira en otro idioma",
         },
         {
-            "label": "français",
-            "code": "fr",
-            "arialabel": "Choisir une autre langue"
+            label: "français",
+            code: "fr",
+            arialabel: "Choisir une autre langue",
         },
         {
-            "label": "हिन्दी",
-            "code": "hi",
-            "arialabel": "अन्य भाषाओं में देखें"
+            label: "हिन्दी",
+            code: "hi",
+            arialabel: "अन्य भाषाओं में देखें",
         },
         {
-            "label": "italiano",
-            "code": "it",
-            "arialabel": "Vedi in altre lingue"
+            label: "italiano",
+            code: "it",
+            arialabel: "Vedi in altre lingue",
         },
         {
-            "label": "नेपाली",
-            "code": "ne",
-            "arialabel": "अन्य भाषामा हेर्नुहोस्"
+            label: "नेपाली",
+            code: "ne",
+            arialabel: "अन्य भाषामा हेर्नुहोस्",
         },
         {
-            "label": "polski",
-            "code": "pl",
-            "arialabel": "Zobacz w innych językach"
+            label: "polski",
+            code: "pl",
+            arialabel: "Zobacz w innych językach",
         },
         {
-            "label": "português brasileiro",
-            "code": "pt_BR",
-            "arialabel": "Ver em outro idioma"
+            label: "português brasileiro",
+            code: "pt_BR",
+            arialabel: "Ver em outro idioma",
         },
         {
-            "label": "русский",
-            "code": "ru",
-            "arialabel": "Посмотреть на других языках"
+            label: "русский",
+            code: "ru",
+            arialabel: "Посмотреть на других языках",
         },
         {
-            "label": "українська",
-            "code": "ua",
-            "arialabel": "Переглянути на інших мовах"
+            label: "українська",
+            code: "ua",
+            arialabel: "Переглянути на інших мовах",
         },
         {
-            "label": "日本語",
-            "code": "ja",
-            "arialabel": "他の言語で読む"
+            label: "日本語",
+            code: "ja",
+            arialabel: "他の言語で読む",
         },
         {
-            "label": "ελληνικά",
-            "code": "gr",
-            "arialabel": "Προβολή σε άλλες γλώσσες"
+            label: "ελληνικά",
+            code: "gr",
+            arialabel: "Προβολή σε άλλες γλώσσες",
         },
         {
-            "label": "nederlands",
-            "code": "nl",
-            "arialabel": "Bekijk in andere talen"
+            label: "nederlands",
+            code: "nl",
+            arialabel: "Bekijk in andere talen",
         },
         {
-            "label": "עברית",
-            "code": "he",
-            "arialabel": "צפה בשפות נוספות"
+            label: "עברית",
+            code: "he",
+            arialabel: "צפה בשפות נוספות",
         },
         {
-            "label": "romana",
-            "code": "ro",
-            "arialabel": "Alege o alta limba"
+            label: "romana",
+            code: "ro",
+            arialabel: "Alege o alta limba",
         },
         {
-            "label": "serbia",
-            "code": "sr",
-            "arialabel": "Pokažite na drugim jezicima"
+            label: "serbia",
+            code: "sr",
+            arialabel: "Pokažite na drugim jezicima",
         },
         {
-            "label": "swedish",
-            "code": "sv",
-            "arialabel": "Visa på andra språk"
+            label: "swedish",
+            code: "sv",
+            arialabel: "Visa på andra språk",
         },
         {
-            "label": "ภาษาไทย",
-            "code": "th",
-            "arialabel": "ดูในภาษาอื่น"
+            label: "ภาษาไทย",
+            code: "th",
+            arialabel: "ดูในภาษาอื่น",
         },
         {
-            "label": "中文",
-            "code": "zh",
-            "arialabel": "其他语言"
+            label: "中文",
+            code: "zh",
+            arialabel: "其他语言",
         },
         {
-            "label": "bahasa indonesia",
-            "code": "id",
-            "arialabel": "Lihat di bahasa lain"
+            label: "bahasa indonesia",
+            code: "id",
+            arialabel: "Lihat di bahasa lain",
         },
         {
-            "label": "Türkçe",
-            "code": "tr",
-            "arialabel": "Diğer dillerde görüntüle"
+            label: "Türkçe",
+            code: "tr",
+            arialabel: "Diğer dillerde görüntüle",
         },
         {
-            "label": "한국어",
-            "code": "ko",
-            "arialabel": "다른 언어로 보기"
+            label: "한국어",
+            code: "ko",
+            arialabel: "다른 언어로 보기",
         },
         {
-            "label": "kazakh",
-            "code": "kk",
-            "arialabel": "Басқа тілді таңдау"
-        }
+            label: "kazakh",
+            code: "kk",
+            arialabel: "Басқа тілді таңдау",
+        },
     ],
-    "collaborators": [
+    collaborators: [
         {
-            "name": "Björn Söderqvist",
-            "link": "https://github.com/cybear",
-            "lang": "sv"
+            name: "Björn Söderqvist",
+            link: "https://github.com/cybear",
+            lang: "sv",
         },
         {
-            "name": "Michael Botha",
-            "link": "https://github.com/michaeljabotha",
-            "lang": "af"
+            name: "Michael Botha",
+            link: "https://github.com/michaeljabotha",
+            lang: "af",
         },
         {
-            "name": "Moritz Stückler",
-            "link": "https://github.com/pReya",
-            "lang": "de"
+            name: "Moritz Stückler",
+            link: "https://github.com/pReya",
+            lang: "de",
         },
         {
-            "name": "Daniil Golubev",
-            "link": "https://github.com/dadyarri",
-            "lang": "ru"
+            name: "Daniil Golubev",
+            link: "https://github.com/dadyarri",
+            lang: "ru",
         },
         {
-            "name": "Łukasz Wójcik",
-            "link": "https://github.com/lwojcik",
-            "lang": "pl"
+            name: "Łukasz Wójcik",
+            link: "https://github.com/lwojcik",
+            lang: "pl",
         },
         {
-            "name": "fedemcmac",
-            "link": "https://github.com/fedemcmac",
-            "lang": "it"
+            name: "fedemcmac",
+            link: "https://github.com/fedemcmac",
+            lang: "it",
         },
         {
-            "name": "Michel",
-            "link": "https://github.com/michelc",
-            "lang": "fr"
+            name: "Michel",
+            link: "https://github.com/michelc",
+            lang: "fr",
         },
         {
-            "name": "Andriy Sultanov",
-            "link": "https://github.com/LastGenius-edu",
-            "lang": "ua"
+            name: "Andriy Sultanov",
+            link: "https://github.com/LastGenius-edu",
+            lang: "ua",
         },
         {
-            "name": "Meiko Hori",
-            "link": "https://github.com/meih",
-            "lang": "ja"
+            name: "Meiko Hori",
+            link: "https://github.com/meih",
+            lang: "ja",
         },
         {
-            "name": "Alex Tzimas",
-            "link": "https://github.com/Tzal3x",
-            "lang": "gr"
+            name: "Alex Tzimas",
+            link: "https://github.com/Tzal3x",
+            lang: "gr",
         },
         {
-            "name": "Martijn ten Heuvel",
-            "link": "https://github.com/MartijntenHeuvel",
-            "lang": "nl"
+            name: "Martijn ten Heuvel",
+            link: "https://github.com/MartijntenHeuvel",
+            lang: "nl",
         },
         {
-            "name": "Elad Leev",
-            "link": "https://github.com/eladleev",
-            "lang": "he"
+            name: "Elad Leev",
+            link: "https://github.com/eladleev",
+            lang: "he",
         },
         {
-            "name": "Franco Fantini",
-            "link": "https://github.com/francofantini",
-            "lang": "es"
+            name: "Franco Fantini",
+            link: "https://github.com/francofantini",
+            lang: "es",
         },
         {
-            "name": "Catalina Focsa",
-            "link": "https://github.com/catalinafox",
-            "lang": "ro"
+            name: "Catalina Focsa",
+            link: "https://github.com/catalinafox",
+            lang: "ro",
         },
         {
-            "name": "Davi Alexandre",
-            "link": "https://github.com/davialexandre",
-            "lang": "pt_BR"
+            name: "Davi Alexandre",
+            link: "https://github.com/davialexandre",
+            lang: "pt_BR",
         },
         {
-            "name": "Nemanja Vasić",
-            "link": "https://github.com/GoodbyePlanet",
-            "lang": "sr"
+            name: "Nemanja Vasić",
+            link: "https://github.com/GoodbyePlanet",
+            lang: "sr",
         },
         {
-            "name": "Tao Jiayuan",
-            "link": "https://github.com/taojy123",
-            "lang": "zh"
+            name: "Tao Jiayuan",
+            link: "https://github.com/taojy123",
+            lang: "zh",
         },
         {
-            "name": "Eduard Tomek",
-            "link": "https://github.com/edee111",
-            "lang": "cs"
+            name: "Eduard Tomek",
+            link: "https://github.com/edee111",
+            lang: "cs",
         },
         {
-            "name": "Ricky Gultom",
-            "link": "https://github.com/quellcrist-falconer",
-            "lang": "id"
+            name: "Ricky Gultom",
+            link: "https://github.com/quellcrist-falconer",
+            lang: "id",
         },
         {
-            "name": "Khaja Md Sher E Alam",
-            "link": "https://github.com/sheralam",
-            "lang": "bn"
+            name: "Khaja Md Sher E Alam",
+            link: "https://github.com/sheralam",
+            lang: "bn",
         },
         {
-            "name": "Rahul Dahal",
-            "link": "https://github.com/rahuldahal",
-            "lang": "ne"
+            name: "Rahul Dahal",
+            link: "https://github.com/rahuldahal",
+            lang: "ne",
         },
         {
-            "name": "Aryan Sarkar",
-            "link": "https://github.com/aryansarkar13",
-            "lang": "hi"
+            name: "Aryan Sarkar",
+            link: "https://github.com/aryansarkar13",
+            lang: "hi",
         },
         {
-            "name": "Taha Paksu",
-            "link": "https://github.com/tpaksu",
-            "lang": "tr"
+            name: "Taha Paksu",
+            link: "https://github.com/tpaksu",
+            lang: "tr",
         },
         {
-            "name": "Kitt Tientanopajai",
-            "link": "https://github.com/kitt-tientanopajai",
-            "lang": "th"
+            name: "Kitt Tientanopajai",
+            link: "https://github.com/kitt-tientanopajai",
+            lang: "th",
         },
         {
-            "name": "Gyeongjae Choi",
-            "link": "https://github.com/ryanking13",
-            "lang": "ko"
+            name: "Gyeongjae Choi",
+            link: "https://github.com/ryanking13",
+            lang: "ko",
         },
         {
-            "name": "Zhunisali Shanabek",
-            "link": "https://github.com/zshanabek",
-            "lang": "kk"
-        }
+            name: "Zhunisali Shanabek",
+            link: "https://github.com/zshanabek",
+            lang: "kk",
+        },
     ],
-    "otherhelpers": [
+    otherhelpers: [
         {
-            "name": "Iain Murray",
-            "link": "https://github.com/imurray",
+            name: "Iain Murray",
+            link: "https://github.com/imurray",
         },
         {
-            "name": "Frank Taillandier",
-            "link": "https://github.com/DirtyF",
+            name: "Frank Taillandier",
+            link: "https://github.com/DirtyF",
         },
         {
-            "name": "David Fyffe",
-            "link": "https://github.com/davidfyffe",
+            name: "David Fyffe",
+            link: "https://github.com/davidfyffe",
         },
         {
-            "name": "Lucas Larson",
-            "link": "https://github.com/LucasLarson",
+            name: "Lucas Larson",
+            link: "https://github.com/LucasLarson",
         },
         {
-            "name": "Artem Vorotnikov",
-            "link": "https://github.com/vorot93",
+            name: "Artem Vorotnikov",
+            link: "https://github.com/vorot93",
         },
         {
-            "name": "Myrzabek Azil",
-            "link": "https://github.com/mvrzvbvk"
-        }
-    ]
+            name: "Myrzabek Azil",
+            link: "https://github.com/mvrzvbvk",
+        },
+    ],
 };
